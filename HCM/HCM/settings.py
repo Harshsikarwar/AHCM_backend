2555bfdbe82ce4275c407885f06b408d3fb88222
Harsh Singh Sikarwar <harshsikarwar2005@gmail.com>
2026-07-05 14:54:20 +0530

    deploying

--- HCM/HCM/settings.py
+++ HCM/HCM/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     'accounts',
+    'ai_features',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
